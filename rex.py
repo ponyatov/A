caacb00c23ba0f0d49949c52e4ccab0afb6ceb41
Dmitry Ponyatov <dponyatov@gmail.com>
2014-12-08 14:16:52 +0300

    + azlin rex files
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- rex.py
+++ rex.py
@@ -3,11 +3,18 @@ print time.localtime()[:6], sys.argv
 
 FILES = {
 	'ide/eclipse_blackbox.menu':[
-		(
-			'../.blackboxmenu',
-			r'\[begin\].+|.+eclipse.+'
-		)
-	]
+		('../.blackboxmenu', r'\[begin\].+|.+eclipse.+')
+	],
+	'azlin/doc/dirs.mk':[('azlin/mk/dirs.mk',r'.+')],
+	'azlin/doc/gz.dirs':[('azlin/mk/dirs.mk',r'GZ .+')],
+	'azlin/doc/src.dirs':[('azlin/mk/dirs.mk',r'SRC .+')],
+	'azlin/doc/tmp.dirs':[('azlin/mk/dirs.mk',r'TMP .+')],
+	'azlin/doc/build.dirs':[('azlin/mk/dirs.mk',r'BUILD .+')],
+	'azlin/doc/tc.dirs':[('azlin/mk/dirs.mk',r'TC .+')],
+	'azlin/doc/root.dirs':[('azlin/mk/dirs.mk',r'ROOT .+'),
+						   ('azlin/mk/dirs.mk',r'BOOT .+')],
+	'azlin/doc/dirs.dirs':[('azlin/mk/dirs.mk',r'DIRS .+')],
+	'azlin/doc/app.mk':[('azlin/mk/head.mk',r'APP .+')]
 }
 
 for i in FILES:
